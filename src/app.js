da9de683403ee8a241abcadf1d2f4ee65cd22875
Gustavo Silva <gstvds@icloud.com>
2020-06-15 22:16:43 -0300

    feat: Adds all routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const repository = { id: uuid(), title,  url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ error: 'Invalid repository ID' });
+
+  const repository = {
+    id,
+    title: title || repositories[repositoryIndex].title,
+    url: url || repositories[repositoryIndex].url,
+    techs: techs || repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ error: 'Invalid repository ID' });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ error: 'Invalid repository ID' });
+
+  let likes = repositories[repositoryIndex].likes
+  likes = likes + 1;
+
+  repositories[repositoryIndex].likes = likes;
+
+  return response.json({ likes });
 });
 
 module.exports = app;
